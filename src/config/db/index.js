62c5b15c728bee47c8dea451ed998331e73622a6
VietAnh <vanh260703@gmail.com>
2025-04-24 16:44:19 +0700

    Update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config(); // Load environment variables from .env file    
+
 const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/users');
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Database connected successfully!');
     } catch (error) {
         console.error('Database connection failed:', error);
